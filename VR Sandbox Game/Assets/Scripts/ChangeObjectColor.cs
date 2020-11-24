446655fc944d1c9a67f9aaab1d3d143088081355
Maurizio <32602950+MaurizioAlt@users.noreply.github.com>
2020-11-24 16:05:32 -0500

    Position Menu v1

---
+++ VR Sandbox Game/Assets/Scripts/ChangeObjectColor.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ChangeObjectColor : MonoBehaviour
+{
+    public GameObject menu;
+    public GameObject menu2;
+    public static GameObject selectedGameObject;
+    public Color[] colors;
+    void Update()
+    {
+        if( Input.GetMouseButtonDown(0) )
+        {
+            Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+            RaycastHit hit;
+            
+            // check if clicked on an object directly
+            if( Physics.Raycast(ray, out hit, 100) && !EventSystem.current.IsPointerOverGameObject())
+            {
+                // Debug.Log( hit.transform.gameObject.name );
+                // if clicked on one of the prefab objects, set the current object to the most recent clicked one
+                if(ObjectList.objects.Contains(hit.transform.gameObject.name)) 
+                {
+                    selectedGameObject = hit.transform.gameObject;
+                    Debug.Log( "Hooray" + selectedGameObject.name);
+                    showColorMenu();
+                }
+            }
+        }
+    }
+
+    public void showColorMenu() 
+    { 
+        menu.SetActive(true);
+    }
+
+    public void setColor(int i)
+    {
+        setColor(colors[i]);
+    }
+
+    void setColor(Color color)
+    {
+        selectedGameObject.GetComponent<Renderer>().material.color = color;
+    }
+
+    public void ExistColorChangeMenu()
+    {
+        menu.SetActive(false);
+    }
+
+}
---
+++ VR Sandbox Game/Assets/Scripts/ChangePosition.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ChangePosition : MonoBehaviour
+{
+    public GameObject menu;
+    public static GameObject selectedGameObject;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            // check if clicked on an object directly
+            if (Physics.Raycast(ray, out hit, 100) && !EventSystem.current.IsPointerOverGameObject())
+            {
+                // Debug.Log( hit.transform.gameObject.name );
+                // if clicked on one of the prefab objects, set the current object to the most recent clicked one
+                if (ObjectList.objects.Contains(hit.transform.gameObject.name))
+                {
+                    selectedGameObject = hit.transform.gameObject;
+                    Debug.Log("Hooray" + selectedGameObject.name);
+                    showPositionMenu();
+                }
+            }
+        }
+    }
+    public void showPositionMenu()
+    {
+        menu.SetActive(true);
+    }
+
+    public void increasePositionX()
+    {
+        selectedGameObject.transform.position += new Vector3(1, 0, 0);
+    }
+    public void increasePositionY()
+    {
+        selectedGameObject.transform.position += new Vector3(0, 1, 0);
+    }
+    public void increasePositionZ()
+    {
+        selectedGameObject.transform.position += new Vector3(0, 0, 1);
+    }
+    public void decreasePositionX()
+    {
+        selectedGameObject.transform.position += new Vector3(-1, 0, 0);
+    }
+    public void decreasePositionY()
+    {
+        selectedGameObject.transform.position += new Vector3(0, -1, 0);
+    }
+    public void decreasePositionZ()
+    {
+        selectedGameObject.transform.position += new Vector3(0, 0, -1);
+    }
+
+    public void ExitPositionChangeMenu()
+    {
+        menu.SetActive(false);
+    }
+}
