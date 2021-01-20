3a51a2be6dce2555a7e80a7c5d7a6fdd3c6e96bc
Abraham Gutierrez <abeg777499@gmail.com>
2021-01-19 17:13:36 -0800

    Teleportation, 2D/3D selection, Laser pointer
    Implemented teleportation, canvas object selection, and 3d object selection. Implemented laser pointer.

---
+++ VR Sandbox Game/Assets/Scripts/ShowControllers.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class ShowControllers : MonoBehaviour
+{
+    public bool showController = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        foreach (var hand in Player.instance.hands)
+        {
+            if (showController)
+            {
+                hand.ShowController();
+            }
+            else
+            {
+                hand.HideController();
+            }
+        }
+        
+    }
+}
