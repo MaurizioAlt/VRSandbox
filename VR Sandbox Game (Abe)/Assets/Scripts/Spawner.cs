3d32dae41dd3ba381195d4e62e58cfe48530824f
Abraham Gutierrez <abeg777499@gmail.com>
2021-02-02 21:42:40 -0800

    Added spawning with vr pointer

--- VR Sandbox Game (Abe)/Assets/Scripts/Spawner.cs
+++ VR Sandbox Game (Abe)/Assets/Scripts/Spawner.cs
@@ -1,19 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
-public class Spawner : MonoBehaviour
+
+
+    public class Spawner : MonoBehaviour
 {
     public GameObject mainmenu;
     public GameObject spawnmenu;
     public GameObject[] spawnObjects;
-    
+    private int index;
+    public GameObject m_Pointer;
+
+    public SteamVR_Action_Boolean m_SpawnObject;
+
+    private SteamVR_Behaviour_Pose m_Pose = null;
+    private bool m_HasPosition = false;
+
     // forward spawning distance based on camera
     public int forwardDistance = 5;
 
-    public void spawn(int i)
+    private void Awake()
     {
-        Instantiate(spawnObjects[i], Camera.main.transform.position + Camera.main.transform.forward * forwardDistance, Camera.main.transform.rotation);
+        m_Pose = GetComponent<SteamVR_Behaviour_Pose>();
+    }
+    
+    private void Update()
+    {
+        //pointer
+        m_HasPosition = UpdatePointer();
+        m_Pointer.SetActive(m_HasPosition);
+
+        //Teleport
+        if (m_SpawnObject.GetLastStateUp(m_Pose.inputSource))
+            spawn();
+
+    }
+ 
+    public void spawn()
+    {
+        Debug.Log("Tried spawning object");
+        Vector3 pointerPosition = new Vector3(m_Pointer.transform.position.x, m_Pointer.transform.position.y+(spawnObjects[0].transform.localScale.y)/2, m_Pointer.transform.position.z);
+
+        Instantiate(spawnObjects[0], pointerPosition, m_Pointer.transform.rotation);
+    }
+
+    public void setIndex(int i)
+    {
+        index = i;
     }
 
     public void EnterMainMenu()
@@ -31,5 +66,23 @@ public class Spawner : MonoBehaviour
     {
         mainmenu.SetActive(true);
         spawnmenu.SetActive(false);
+    }
+
+    private bool UpdatePointer()
+    {
+        //Ray from controller
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        //If its a hit
+        if (Physics.Raycast(ray, out hit))
+        {
+            m_Pointer.transform.position = hit.point;
+            return true;
+        }
+
+
+        //If not a hit
+        return false;
     }
 }
