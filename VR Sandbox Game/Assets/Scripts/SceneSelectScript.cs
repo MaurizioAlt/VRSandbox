b6f7ebb857d9c296bec4c967b30bb5934980ac00
Zactyz <zachtyz@knights.ucf.edu>
2021-02-12 09:43:59 -0500

    Scenes Menu
    created not working scenes menu

---
+++ VR Sandbox Game/Assets/Scripts/SceneSelectScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public void selectScene()
+    {
+        switch (this.gameObject.name) 
+        {
+            case "Plain"
+                SceneManager.LoadScene("");
+                break;
+            case "Space"
+                SceneManager.LoadScene("");
+                break;
+            case "Forest"
+                SceneManager.LoadScene("");
+                break;
+        }
+    }
+}
