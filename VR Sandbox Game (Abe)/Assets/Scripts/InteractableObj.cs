2c0a356482f319961851f5b71780151277102496
Abraham Gutierrez <abeg777499@gmail.com>
2021-02-25 21:56:54 -0800

    Fixed Distance Grabbing/Added rotation basis

--- VR Sandbox Game (Abe)/Assets/Scripts/InteractableObj.cs
+++ VR Sandbox Game (Abe)/Assets/Scripts/InteractableObj.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class InteractableObj : MonoBehaviour
 {
-    [HideInInspector]
-    public HandGrabbing m_ActiveHand = null;
+   // [HideInInspector]
+   // public HandGrabbing m_ActiveHand = null;
+   // public RotateObject m_ActiveHand = null;
 }
--- VR Sandbox Game (Abe)/Assets/Scripts/RotateObject.cs
+++ VR Sandbox Game (Abe)/Assets/Scripts/RotateObject.cs
@@ -3,19 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR;
 
-public class HandGrabbing : MonoBehaviour
+public class RotateObject : MonoBehaviour
 {
     private bool m_HasPosition = false;
-    public SteamVR_Action_Boolean m_GrabAction = null;
+    public SteamVR_Action_Boolean m_RotateAction = null;
 
     private SteamVR_Behaviour_Pose m_Pose = null;
-    //private FixedJoint m_Joint = null;
+    private FixedJoint m_Joint = null;
 
     public GameObject movePointer;
-    private bool grabbingObj = false;
-    private bool foundInteractable = false;
-
-    public static GameObject interactable;
+    private bool rotatingObj = false;
 
     public static InteractableObj m_CurrentInteractiable = null;
     public static List<InteractableObj> m_ContactInteractables = new List<InteractableObj>();
@@ -24,8 +21,8 @@ public class HandGrabbing : MonoBehaviour
     private void Awake()
     {
         m_Pose = GetComponent<SteamVR_Behaviour_Pose>();
-       // m_Joint = GetComponent<FixedJoint>();
-        //m_Joint.connectedBody = movePointer.GetComponent<Rigidbody>();
+        m_Joint = GetComponent<FixedJoint>();
+        m_Joint.connectedBody = movePointer.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -34,48 +31,43 @@ public class HandGrabbing : MonoBehaviour
         m_HasPosition = UpdatePointer();
         movePointer.SetActive(m_HasPosition);
 
-         if (grabbingObj)
-            {
-            interactable.transform.position = movePointer.transform.position;
-            }
-
-        if (m_GrabAction.GetState(m_Pose.inputSource) && foundInteractable)
+        if (rotatingObj)
         {
-             Pickup();
-             Debug.Log("pickup");
-        }
-        else
-        {
-            Drop();
+            m_CurrentInteractiable.transform.rotation = transform.rotation;
         }
 
-            //  if (m_GrabAction.GetStateUp(m_Pose.inputSource))
-            //  {
-            //     Drop();
-            //    Debug.Log("Down");
-            //   }
+        if (m_RotateAction.GetStateDown(m_Pose.inputSource))
+        {
+            beginRotation();
+            Debug.Log("Up");
+
+        }
+
+        if (m_RotateAction.GetStateUp(m_Pose.inputSource))
+        {
+            endRotation();
+            Debug.Log("Down");
+        }
     }
 
 
 
-    public void Pickup()
+    public void beginRotation()
     {
         //Get nearest itneractable
-       // m_CurrentInteractiable = GetNearestInteractable();
+        m_CurrentInteractiable = GetNearestInteractable();
 
         //null check
-       // if (!m_CurrentInteractiable)
-         //   return;
+        if (!m_CurrentInteractiable)
+            return;
 
         //Already held check
-       // if (m_CurrentInteractiable.m_ActiveHand)
-            //m_CurrentInteractiable.m_ActiveHand.Drop();
+      //  if (m_CurrentInteractiable.m_ActiveHand)
+         //   m_CurrentInteractiable.m_ActiveHand.Drop();
 
 
-        grabbingObj = true;
-        movePointer.transform.position = interactable.transform.position;
+        rotatingObj = true;
         movePointer.transform.parent = transform;
-        //interactable.transform.parent = transform;
 
         //Attach
         //Rigidbody targetBody = m_CurrentInteractiable.GetComponent<Rigidbody>();
@@ -89,11 +81,11 @@ public class HandGrabbing : MonoBehaviour
        // m_CurrentInteractiable.m_ActiveHand = this;
     }
 
-    public void Drop()
+    public void endRotation()
     {
         //null check
-        //if (!m_CurrentInteractiable)
-          //  return;
+        if (!m_CurrentInteractiable)
+            return;
 
         //Apply velocity
         //Rigidbody targetBody = m_CurrentInteractiable.GetComponent<Rigidbody>();
@@ -102,12 +94,12 @@ public class HandGrabbing : MonoBehaviour
         //m_Joint.connectedBody = null;
         //m_CurrentInteractiable.transform.parent = null;
 
-        grabbingObj = false;
+        rotatingObj = false;
         movePointer.transform.parent = null;
 
         //Clear
-       // m_CurrentInteractiable.m_ActiveHand = null;
-      //  m_CurrentInteractiable = null;
+      //  m_CurrentInteractiable.m_ActiveHand = null;
+        m_CurrentInteractiable = null;
     }
 
     private InteractableObj GetNearestInteractable()
@@ -117,11 +109,11 @@ public class HandGrabbing : MonoBehaviour
         float minDistance = float.MaxValue;
         float distance = 0.0f;
 
-        foreach(InteractableObj interactable in m_ContactInteractables)
+        foreach (InteractableObj interactable in m_ContactInteractables)
         {
             distance = (interactable.transform.position - movePointer.transform.position).sqrMagnitude;
 
-            if(distance < minDistance)
+            if (distance < minDistance)
             {
                 minDistance = distance;
                 nearest = interactable;
@@ -138,21 +130,9 @@ public class HandGrabbing : MonoBehaviour
         RaycastHit hit;
 
         //If its a hit
-        if (Physics.Raycast(ray, out hit) && !grabbingObj)
+        if (Physics.Raycast(ray, out hit) && !rotatingObj)
         {
-            if (hit.collider.gameObject.CompareTag("InteractableObject"))
-            {
-                foundInteractable = true;
-                interactable = hit.collider.gameObject;
-                Debug.Log("Found interactable");
-
-            }
-            else
-            {
-                foundInteractable = false;
-                interactable = null;
-                Debug.Log("Not interactable");
-            }
+            movePointer.transform.position = hit.point;
             return true;
         }
 
